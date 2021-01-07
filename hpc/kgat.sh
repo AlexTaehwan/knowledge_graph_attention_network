659d31160f425305389e1fc314877e0f97d70f18
Colby Ham <colby.ham@pnnl.gov>
2021-01-07 15:12:23 -0800

    Update kgat script

--- hpc/kgat.sh
+++ hpc/kgat.sh
@@ -1,4 +1,42 @@
-# Not complete, uploading to test forked repo
+#!/usr/bin/bash
+
+#SBATCH --job-name=kgat
+#SBATCH --output=logs/%x-%j.out
+#SBATCH -A st_graphs
+#SBATCH -p dlt
+#SBATCH -n 1
+#SBATCH --gres=gpu:1
+#SBATCH -t 47:59:00
+
+module purge
+module load cuda/9.2.148 
+module load python/anaconda3.2019.3
+module load gcc/5.2.0
+source /share/apps/python/anaconda3.2019.3/etc/profile.d/conda.sh
+source activate kgat
+
+model_type=kgat
+alg_type=bi
+dataset=yelp2018
+
 REPO_DIR=~/recommendation/knowledge_graph_attention_network
-cd $REPO_DIR
-python Main.py --model_type kgat --alg_type bi --dataset yelp2018 --regs [1e-5,1e-5] --layer_size [64,32,16] --embed_size 64 --lr 0.0001 --epoch 1000 --verbose 50 --save_flag 1 --pretrain -1 --batch_size 1024 --node_dropout [0.1] --mess_dropout [0.1,0.1,0.1] --use_att True --use_kge True
+MODEL_DIR=${REPO_DIR}/Model
+cd $MODEL_DIR
+
+python Main.py \
+    --model_type $model_type \
+    --alg_type $alg_type \
+    --dataset $dataset \
+    --regs [1e-5,1e-5] \
+    --layer_size [64,32,16] \
+    --embed_size 64 \
+    --lr 0.0001 \
+    --epoch 1000 \
+    --verbose 50 \
+    --save_flag 1 \
+    --pretrain -1 \
+    --batch_size 1024 \
+    --node_dropout [0.1] \
+    --mess_dropout [0.1,0.1,0.1] \
+    --use_att True \
+    --use_kge True
