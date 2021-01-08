4487ef8115d1fcf6457780969f2fea96d34cffcd
tk020 <tk020@bucknell.edu>
2021-01-08 14:43:41 -0500

    sh file

--- hpc/kgat.sh
+++ hpc/kgat.sh
@@ -15,28 +15,30 @@ module load gcc/5.2.0
 source /share/apps/python/anaconda3.2019.3/etc/profile.d/conda.sh
 source activate kgat
 
-model_type=kgat
-alg_type=bi
-dataset=yelp2018
-
 REPO_DIR=~/recommendation/knowledge_graph_attention_network
 MODEL_DIR=${REPO_DIR}/Model
 cd $MODEL_DIR
 
-python Main.py \
-    --model_type $model_type \
-    --alg_type $alg_type \
-    --dataset $dataset \
-    --regs [1e-5,1e-5] \
-    --layer_size [64,32,16] \
-    --embed_size 64 \
-    --lr 0.0001 \
-    --epoch 1000 \
-    --verbose 50 \
-    --save_flag 1 \
-    --pretrain -1 \
-    --batch_size 1024 \
-    --node_dropout [0.1] \
-    --mess_dropout [0.1,0.1,0.1] \
-    --use_att True \
-    --use_kge True
+for model_type in {kgat, bprmf, fm, nfm, cke, cfkg}
+do
+    for dataset in {yelp2018, amazon-book, last-fm}
+    do
+        python Main.py \
+            --model_type $model_type \
+            --alg_type bi \
+            --dataset $dataset \
+            --regs [1e-5,1e-5] \
+            --layer_size [64,32,16] \
+            --embed_size 64 \
+            --lr 0.0001 \
+            --epoch 1000 \
+            --verbose 50 \
+            --save_flag 1 \
+            --pretrain -1 \
+            --batch_size 1024 \
+            --node_dropout [0.1] \
+            --mess_dropout [0.1,0.1,0.1] \
+            --use_att True \
+            --use_kge True
+    done
+done
